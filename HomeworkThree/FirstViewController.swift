604a6e3b9eed566f66ba607e2dc5fd9554454431
wilson <wilson@aja.com.tw>
2017-10-28 04:33:54 +0800

    okay

--- HomeworkThree/FirstViewController.swift
+++ HomeworkThree/FirstViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
+    @IBOutlet weak var poster: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
@@ -26,3 +28,22 @@ class FirstViewController: UIViewController {
 
 }
 
+class PosterViewController: UIViewController {
+    
+    @IBOutlet weak var poster: UIImageView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidLoad()
+        
+        UIImageView.animate(withDuration: 3, delay: 0.2, animations: {
+            self.poster.alpha = 0.8
+        })
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
