676bdeba58ec67903b4eefc94059509a6e3f90d6
wilson <wilson@aja.com.tw>
2017-10-27 10:02:28 +0800

    Initial Commit

---
+++ HomeworkThree/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HomeworkThree
+//
+//  Created by wilson on 2017/10/27.
+//  Copyright © 2017年 wilson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
